ae03d5aafe6ea4b25e523545075579d0339fdb2b
PW <pwelliver@yahoo.com>
2019-09-08 13:52:48 +0200

    UI is started

---
+++ models/GoogleUser.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema for Google user
+// I named it GoogleUser because we already have a user table
+// Rename later ...
+const GoogleUserSchema = new Schema({
+  googleID: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+});
+
+// Create collection and add schema
+// Change to users or google_users
+mongoose.model('google_users', GoogleUserSchema); 
